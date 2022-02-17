bc980e551e2fcd4a895f103df855895c10d11c4f
Chris Scott <chris@chrisdjscott.co.uk>
2022-02-17 16:46:31 +1300

    revert parsl

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         "requests",
         "fair-research-login",
         "globus-sdk>=3",
-        "parsl==1.2.0",  # not actually used directly but possibly required, match version from funcx-endpoint
         "funcx==0.3.6",
     ],
     classifiers=[
