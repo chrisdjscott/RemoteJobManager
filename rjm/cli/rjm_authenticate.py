eee32542f5801e501620c2460e39e9acfa984368
Chris Scott <chris@chrisdjscott.co.uk>
2022-02-18 15:21:46 +1300

    rjm_authenticate doesn't print anything by default

--- rjm/cli/rjm_authenticate.py
+++ rjm/cli/rjm_authenticate.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 import argparse
 
 from rjm.remote_job import RemoteJob
@@ -13,6 +14,7 @@ def make_parser():
     parser = argparse.ArgumentParser(description="Perform required authentication (if any)")
     parser.add_argument('--force', action="store_true",
                         help="Delete any stored tokens and force reauthentication")
+    parser.add_argument("--verbose", action="store_true", help="Enable print statements")
     parser.add_argument("-v", '--version', action='version', version='%(prog)s ' + __version__)
 
     return parser
@@ -27,24 +29,34 @@ def authenticate():
     parser = make_parser()
     args = parser.parse_args()
 
-    print("Authenticating RJM...")
+    if args.verbose:
+        print("Authenticating RJM...")
 
     # check config file exists (configure should have been done already)
     if not os.path.isfile(config_helper.CONFIG_FILE_LOCATION):
-        raise RuntimeError("rjm configure must be run before authenticate")
+        sys.stderr.write("ERROR: configuration file must be create with rjm_configure before running rjm_authenticate" + os.linesep)
+        sys.exit(1)
 
     # delete token file if exists
     if args.force:
         if os.path.isfile(utils.TOKEN_FILE_LOCATION):
-            print(f"Deleting existing token file to force reauthentication: {utils.TOKEN_FILE_LOCATION}")
+            if args.verbose:
+                print(f"Deleting existing token file to force reauthentication: {utils.TOKEN_FILE_LOCATION}")
             os.unlink(utils.TOKEN_FILE_LOCATION)
 
     # just for first directory should be ok
     rj = RemoteJob()
     globus_scopes = rj.get_required_globus_scopes()
-    print("Requesting authentication - will open link in web browser if required...")
-    utils.handle_globus_auth(globus_scopes)
-    print("RJM authentication completed")
+    if args.verbose:
+        print("Requesting authentication - will open link in web browser if required...")
+    try:
+        utils.handle_globus_auth(globus_scopes)
+    except Exception as exc:
+        sys.stderr.write(f"ERROR: failed to do Globus auth: {exc}" + os.linesep)
+        sys.exit(1)
+    else:
+        if args.verbose:
+            print("RJM authentication completed")
 
 
 if __name__ == "__main__":
