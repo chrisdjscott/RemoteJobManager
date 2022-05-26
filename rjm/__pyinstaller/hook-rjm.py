af9c1f9bbd53acb5f317d8e9b3af80a2d87070b4
Chris Scott <chris@chrisdjscott.co.uk>
2022-05-26 20:26:22 +1200

    forcing persist script to be included by pyinstaller

--- rjm/__pyinstaller/hook-rjm.py
+++ rjm/__pyinstaller/hook-rjm.py
@@ -1,4 +1,6 @@
+import os
 from PyInstaller.utils.hooks import collect_data_files
 
 # we have to include_py_files so that funcx can extract function source
 datas = collect_data_files('rjm', include_py_files=True, excludes=['__pyinstaller'])
+datas += [(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "setup", "funcx-endpoint-persist-nesi.sh"), os.path.join("rjm", "setup", "funcx-endpoint-persist-nesi.sh"))]
