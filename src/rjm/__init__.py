5556f7aa86c5215a3a00be1f819d9885996ef889
Chris Scott <chris@chrisdjscott.co.uk>
2023-04-21 14:20:35 +1200

    switch to pyproject.toml and move package to src

---
+++ src/rjm/__init__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("RemoteJobManager")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
