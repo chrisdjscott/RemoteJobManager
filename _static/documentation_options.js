86b8aabf85e9c1add810489c7da57b7b54269489
chrisdjscott <chrisdjscott@users.noreply.github.com>
2023-05-07 23:35:36 +0000

    deploy: 2ffbdecf68ad2c2e9d90635f4472231d4b4dcad4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.1.dev20+ge6c5645',
+    VERSION: '0.11.1.dev22+g2ffbdec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
